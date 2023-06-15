9211e97a064dd8a2acc568f078439f5a492459c0
Maires Souza <trabalho.maires@gmail.com>
2023-06-15 11:47:07 -0300

    feat: add pixel-board

--- script.js
+++ script.js
@@ -10,15 +10,26 @@ const colorPalette = document.createElement('ul');
 colorPalette.id = 'color-palette';
 parent.appendChild(colorPalette);
 //  Create colors
-const creatColor = (className) => {
-  const newColor = document.createElement('li');
-  newColor.className = className;
-  return newColor;
+const creatSquares = (className) => {
+  const newSquare = document.createElement('li');
+  newSquare.className = className;
+  return newSquare;
 };
-// Add colors
+// Add squares of palette colors
 const addColor = (parentElement, child) => parentElement.appendChild(child);
+addColor(colorPalette, creatSquares('color red'));
+addColor(colorPalette, creatSquares('color green'));
+addColor(colorPalette, creatSquares('color yellow'));
+addColor(colorPalette, creatSquares('color blue'));
+// Create pixels board
+const pixelBoard = document.createElement('section');
+pixelBoard.id = 'pixel-board';
+parent.appendChild(pixelBoard);
+const lineBreak = document.createElement('br');
 
-addColor(colorPalette, creatColor('color red'));
-addColor(colorPalette, creatColor('color green'));
-addColor(colorPalette, creatColor('color yellow'));
-addColor(colorPalette, creatColor('color blue'));
+for (let i = 0; i < 5; i += 1) {
+  for (let j = 0; j < 5; j += 1) {
+    addColor(pixelBoard, creatSquares('pixel'));
+  }
+  colorPalette.appendChild(lineBreak);
+}
